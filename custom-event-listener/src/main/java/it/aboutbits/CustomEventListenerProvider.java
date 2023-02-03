bc01677020166e704a308dd0d7392adb609914e6
gmarcha <gmarcha@github.com>
2023-02-03 16:55:20 +0000

    Add login and register event handlers

--- custom-event-listener/src/main/java/it/aboutbits/CustomEventListenerProvider.java
+++ custom-event-listener/src/main/java/it/aboutbits/CustomEventListenerProvider.java
@@ -1,6 +1,7 @@
 package it.aboutbits;
 
 import org.jboss.logging.Logger;
+import org.keycloak.broker.provider.util.SimpleHttp;
 import org.keycloak.email.DefaultEmailSenderProvider;
 import org.keycloak.email.EmailException;
 import org.keycloak.events.Event;
@@ -11,6 +12,8 @@ import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.RealmModel;
 import org.keycloak.models.RealmProvider;
 import org.keycloak.models.UserModel;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class CustomEventListenerProvider implements EventListenerProvider {
 
@@ -27,6 +30,43 @@ public class CustomEventListenerProvider implements EventListenerProvider {
     @Override
     public void onEvent(Event event) {
 
+        System.out.println("Hey, I'm in the onEvent method!");
+
+        if (EventType.LOGIN.equals(event.getType())) {
+            log.infof("## NEW %s EVENT", event.getType());
+            log.info("-----------------------------------------------------------");
+
+            RealmModel realm = this.model.getRealm(event.getRealmId());
+            UserModel newRegisteredUser = this.session.users().getUserById(realm, event.getUserId());
+
+            log.infof("## USER ID: %s, USERNAME: %s, EMAIL: %s, FIRST NAME: %s, LAST NAME: %s", newRegisteredUser.getId(), newRegisteredUser.getUsername(), newRegisteredUser.getEmail(), newRegisteredUser.getFirstName(), newRegisteredUser.getLastName());
+            log.info("-----------------------------------------------------------");
+
+            try {
+              String query = "mutation insertUser ($user: user_insert_input!) { insert_user_one(object: $user, on_conflict: { constraint: user_pkey, update_columns: [username, email, displayname] }) { id }}";
+              String userObject = "{\"id\":\"" + newRegisteredUser.getId() + "\", \"username\":\"" + newRegisteredUser.getUsername() + "\", \"email\":\"" + newRegisteredUser.getEmail() + "\"}";
+              String userMutation = "{\"query\":\"" + query + "\",\"variables\":{\"user\":" + variables + "}}";
+              Object requestBody = new ObjectMapper().readValue(userMutation, Object.class);
+              SimpleHttp request = SimpleHttp.doPost("http://hasura.hasura/v1/graphql", session).json(requestBody);
+              // if (sharedSecret != null) {
+              //   request.header(
+              //       "X-Keycloak-Signature",
+              //       hmacFor(task.getEvent(), sharedSecret, algorithm.orElse(HMAC_SHA256_ALGORITHM)));
+              // }
+              SimpleHttp.Response response = request.asResponse();
+              int status = response.getStatus();
+              String body = response.asString();
+
+              log.infof("## HTTP RESPONSE STATUS: %d", status);
+              log.infof("## HTTP RESPONSE BODY: %s", body);
+              log.info("-----------------------------------------------------------");
+            }
+            catch (Exception e) {
+                log.infof("## ERROR: %s", e.getMessage());
+                log.info("-----------------------------------------------------------");
+            }
+        }
+
         if (EventType.REGISTER.equals(event.getType())) {
             log.infof("## NEW %s EVENT", event.getType());
             log.info("-----------------------------------------------------------");
@@ -34,25 +74,7 @@ public class CustomEventListenerProvider implements EventListenerProvider {
             RealmModel realm = this.model.getRealm(event.getRealmId());
             UserModel newRegisteredUser = this.session.users().getUserById(realm, event.getUserId());
 
-            String emailPlainContent = "New user registration\n\n" +
-                    "Email: " + newRegisteredUser.getEmail() + "\n" +
-                    "Username: " + newRegisteredUser.getUsername() + "\n" +
-                    "Client: " + event.getClientId();
-
-            String emailHtmlContent = "<h1>New user registration</h1>" +
-                    "<ul>" +
-                    "<li>Email: " + newRegisteredUser.getEmail() + "</li>" +
-                    "<li>Username: " + newRegisteredUser.getUsername() + "</li>" +
-                    "<li>Client: " + event.getClientId() + "</li>" +
-                    "</ul>";
-
-            DefaultEmailSenderProvider senderProvider = new DefaultEmailSenderProvider(session);
-
-            try {
-                senderProvider.send(session.getContext().getRealm().getSmtpConfig(), "admin@example.com", "Keycloak - New Registration", emailPlainContent, emailHtmlContent);
-            } catch (EmailException e) {
-                log.error("Failed to send email", e);
-            }
+            log.infof("## USER_ID: %s, USERNAME: %s", newRegisteredUser.getId(), newRegisteredUser.getUsername());
             log.info("-----------------------------------------------------------");
         }
 
